c1d62dd5dea0565e0e40d6111022d6f1c19a28f4
Gammpyr <malak93@yandex.ru>
2025-05-08 16:56:59 +0300

    update views.py

---
+++ src/views.py
@@ -0,0 +1,16 @@
+from src.utils import get_greetings_by_time, get_exchange_rate, get_stock_price, get_cards_info, \
+    get_top_transaction_info
+
+
+def web_main():
+    result = {"greeting": get_greetings_by_time(),
+              "cards": get_cards_info(),
+              "top_transactions": get_top_transaction_info(),
+              "currency_rates": get_exchange_rate(),
+              "stock_prices": get_stock_price(),
+              }
+
+    return result
+
+
+print(web_main())
