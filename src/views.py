2296b2b9448106306bdc48f3f52a470f95d255be
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-05-12 21:56:36 +0300

    update views.py

--- src/views.py
+++ src/views.py
@@ -1,3 +1,5 @@
+import json
+
 from src.utils import get_greetings_by_time, get_exchange_rate, get_stock_price, get_cards_info, \
     get_top_transaction_info
 
@@ -5,7 +7,7 @@ def return_df_from_file():
     pass
 
 
-def web_main():
+def web_main(time: str):
     result = {"greeting": get_greetings_by_time(),
               "cards": get_cards_info(),
               "top_transactions": get_top_transaction_info(),
@@ -15,5 +17,6 @@ def web_main():
 
     return result
 
-
-print(web_main())
+with open('returned_data.json', 'w', encoding='utf-8') as file:
+    json.dump(web_main('YYYY-MM-DD HH:MM:SS'), file, ensure_ascii=False, indent=4 )
+# print(web_main())
