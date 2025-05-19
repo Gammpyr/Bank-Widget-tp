7a5fab650575e31e538680c9ca1eb4742e0414a9
Gammpyr <malak93@yandex.ru>
2025-05-19 15:21:32 +0300

    update tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,17 +3,53 @@ from unittest.mock import patch, Mock
 from datetime import datetime
 import pandas as pd
 
+from src import views
 from src.views import main_web, main_events
 
 
-@pytest.fixture
-def mock_data():
-    """Фикстура с тестовыми данными DataFrame"""
-    return pd.DataFrame({
-        "Сумма платежа": [-1000, -2000, 3000, -500],
-        "Статус": ["OK", "OK", "OK", "FAILED"],
-        "Категория": ["Супермаркеты", "Транспорт", "Каршеринг", "Переводы"],
-        "Дата платежа": ["01-01-2023", "15-01-2023", "01-01-2023", "20-01-2023"],
-        "Номер карты": ["1234", "5678", "1234", "5678"],
-        "Описание": ["Магнит", "Такси", "Яндекс-Драйв", "Олег"]
-    })
+
+
+expected = {
+        "greeting": 'Доброй ночи (мок успешен)',
+        "cards": ['Карты и суммы расходов (мок успешен)'],
+        "top_transactions": ['Топ 5 транзакций (мок успешен)'],
+        "currency_rates": ['Курс валюты (мок успешен)'],
+        "stock_prices": ['Курс акций (мок успешен)']
+    }
+
+
+
+@patch.object(views, 'stock_price', ['Курс акций (мок успешен)'])
+@patch.object(views, 'exchange_rate', ['Курс валюты (мок успешен)'])
+@patch('src.views.get_stock_price')
+@patch('src.views.get_exchange_rate')
+@patch('src.views.get_top5_transaction_info')
+@patch('src.views.get_cards_info')
+@patch('src.views.get_greetings_by_time')
+def test_main_web(mock_greeting, mock_cards, mock_top5, mock_exchange, mock_stock):
+    """Тест работоспособности"""
+    mock_greeting.return_value = 'Доброй ночи (мок успешен)'
+    mock_cards.return_value = ['Карты и суммы расходов (мок успешен)']
+    mock_top5.return_value = ['Топ 5 транзакций (мок успешен)']
+    mock_exchange.return_value = ['Курс валюты (мок успешен)']
+    mock_stock.return_value = ['Курс акций (мок успешен)']
+
+    assert main_web('YY-mm', 'df') == expected
+
+
+@patch.object(views, 'stock_price', ['Курс акций (мок успешен)'])
+@patch.object(views, 'exchange_rate', ['Курс валюты (мок успешен)'])
+@patch('src.views.get_stock_price')
+@patch('src.views.get_exchange_rate')
+@patch('src.views.get_top5_transaction_info')
+@patch('src.views.get_cards_info')
+@patch('src.views.get_greetings_by_time')
+def test_main_web(mock_greeting, mock_cards, mock_top5, mock_exchange, mock_stock):
+    """Тест работоспособности"""
+    mock_greeting.return_value = 'Доброй ночи (мок успешен)'
+    mock_cards.return_value = ['Карты и суммы расходов (мок успешен)']
+    mock_top5.return_value = ['Топ 5 транзакций (мок успешен)']
+    mock_exchange.return_value = ['Курс валюты (мок успешен)']
+    mock_stock.return_value = ['Курс акций (мок успешен)']
+
+    assert main_web('YY-mm', 'df') == expected
